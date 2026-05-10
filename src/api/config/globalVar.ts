d2240953b096944222128190dbe8a12d9cb2fe1b
sZmanu <manuellaalvesdeoliveira62@gmail.com>
2026-05-10 11:03:17 -0300

    fix: correção nas variaveis da api

--- src/api/config/globalVar.ts
+++ src/api/config/globalVar.ts
@@ -1,5 +1,5 @@
-const ADDRESS = "localhost";
-const PORT = "3000"
+// const ADDRESS = "localhost";
+// const PORT = "3000"
 
 export const GLOBAL_VAR = {
   BASE_URL: import.meta.env.VITE_BASE_URL ?? 'http://localhost:3000',
